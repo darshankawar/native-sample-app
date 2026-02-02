b5d23810c093b0d042e86304fa12a49df01f4455
Darshan Kawar <darshan@nevercode.io>
2026-02-02 10:45:32 +0530

    Initial Commit

---
+++ sample-native-app/sample_native_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_native_appApp.swift
+//  sample-native-app
+//
+//  Created by Dhs on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct sample_native_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
